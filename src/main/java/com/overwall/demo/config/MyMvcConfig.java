9623f92d72cc44ff926cada46726c3dc1c37d270
mangosata <mangosata@gmail.com>
2020-04-04 02:31:16 -0400

    完成登录界面及国际化

--- src/main/java/com/overwall/demo/config/MyMvcConfig.java
+++ src/main/java/com/overwall/demo/config/MyMvcConfig.java
@@ -1,7 +1,9 @@
 package com.overwall.demo.config;
 
+import com.overwall.demo.component.MyLocalResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -19,5 +21,10 @@ public class MyMvcConfig implements WebMvcConfigurer {
             }
         };
         return adapter;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new MyLocalResolver();
     }
 }
